b24e212229c03c200d771162c5fe9319a6a03290
JesusRL96 <jesusalbertoramirezlopez@gmail.com>
2021-11-25 21:18:35 -0600

    GridLayout

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.anchorlayout import AnchorLayout 
+from kivy.uix.gridlayout import GridLayout 
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
+
+# class GridLayoutExample(GridLayout):
+#     pass
 
 class AnchorLayoutExample(AnchorLayout):
     pass
