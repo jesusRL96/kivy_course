5979d8e3c653ada525bdb449d565e6a42827854e
JesusRL96 <jesusalbertoramirezlopez@gmail.com>
2021-11-25 20:26:11 -0600

    BoxLayout

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+
+class BoxLayoutExample(BoxLayout):
+    pass
+    # def __init__(self, **kwargs):
+    #     super().__init__(**kwargs)
+    #     self.orientation = "vertical"
+
+    #     b1 = Button(text="Button1")
+    #     b2 = Button(text="Button2")
+    #     b3 = Button(text="Button3")
+
+    #     self.add_widget(b1)
+    #     self.add_widget(b2)
+    #     self.add_widget(b3)
 
 class MainWidget(Widget):
     pass
